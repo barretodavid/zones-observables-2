161b38f3862073f32e5725f0142af281a338947d
David Barreto <barretollano@gmail.com>
2016-06-28 11:27:43 -0400

    Initial app commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/Rx';
+
+interface Person {
+  id: number;
+  name: string;
+}
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  template: `
+    <b>Angular 2 HTTP requests using RxJs Observables!</b>
+    <div><code>{{ doctors.toString() }}</code></div>
+  `
 })
 export class AppComponent {
-  title = 'app works!';
+  doctors: any[] = [];
+
+  constructor(http: Http) {
+    http.get('http://jsonplaceholder.typicode.com/users/')
+      .flatMap(data => data.json())
+      .filter((person: Person) => person.id > 5)
+      .map((person: Person) => 'Dr. ' + person.name)
+      .subscribe((data) => {
+        this.doctors.push(data);
+    });
+  }
 }
